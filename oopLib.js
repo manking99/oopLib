89c25e1f6e1b3198e65fde13507d7eed827e3da2
manking99 <manking@list.ru>
2016-04-23 19:56:56 +0400

    new adding

--- oopLib.js
+++ oopLib.js
@@ -1,37 +1,64 @@
 ﻿/**
  * @oopLib Various tool functions.
  * @author <a href="mailto:manking@list.ru">Dmitriy Belonovskiy</a>
- * @version 1.0.0
+ * @version 1.0.1
  */
 window.oopLib = {}; // global object
 
 
+/**
+ * Getting class config if it defined
+ * @param {string} - name of the class
+ */
+function getClassConfig(className) {
+
+    if (!(className in window.oopLib)) {
+        console.error('Class ' + className + " not defined! getClassConfig()");
+        return null;
+    }
+    return window.oopLib[className];
+}
+
+
+/**
+ * Get all parents 
+ * @param {array} - names of parent classes
+ */
+function getAllParents(array_parents, parents, level, order) {
+
+    level++;
+    for (var i = 0; i < array_parents.length; i++) {
+        var cfg = getClassConfig(array_parents[i]);
+
+        order++;
+        parents.push({
+            level: level,
+            order: order,
+            value: array_parents[i]
+        });
+
+        if (cfg.extends) {
+            parents.concat(getAllParents(cfg.extends, parents, level, order));
+        }
+    }
+    return parents;
+}
+
+/**
+ * Defining a class by name
+ * @param {string} - name of the class
+ * @param {Object} - config options of the object. Thay will replace same name options in parent class.
+ */
 function defineClass(className, config) {
 
     if (className in oopLib) {
-        console.error('Class ' + className + " not defined!");
+        console.error('Class ' + className + " already defined!");
         return;
     }
 
-    // 
     if (config.extends) {
-
         if (!Array.isArray(config.extends)) {
             config.extends = [config.extends];
-        }
-
-        for (var i = 0; i < config.extends.length; i++) {
-
-            if (!(config.extends[i] in window.oopLib)) {
-                console.error('Parent class "' + config.extends[i] + '" not found!');
-                throw "oopLib Error!"
-            } else {
-                if (!(className in window.oopLib)) {
-                    window.oopLib[className] = {};
-                }
-
-                Object.assign(oopLib[className], window.oopLib[config.extends[i]]);
-            }
         }
     }
 
@@ -42,6 +69,51 @@ function defineClass(className, config) {
 }
 
 
+function unique(a_, compareFunc) {
+    var a = a_.slice();
+    a.sort(compareFunc);
+    for (var i = 1; i < a.length;) {
+        if (compareFunc(a[i - 1], a[i]) === 0) {
+            a.splice(i, 1);
+        } else {
+            i++;
+        }
+    }
+    return a;
+}
+
+
+
+/**
+ * Mixins configs of the classes to obj
+ * @param {array} - strings - name of the classes
+ * @param {object} - instance of the class
+ */
+function mixinConfig(parentsNames, obj) {
+
+    if (parentsNames) {
+        if (!Array.isArray(parentsNames)) {
+            parentsNames = [parentsNames];
+        }
+
+        for (var i = 0; i < parentsNames.length; i++) {
+            if (!(parentsNames[i] in window.oopLib)) {
+                console.error('Parent class "' + parentsNames[i] + '" not found!');
+                throw "oopLib Error!"
+            } else {
+                Object.assign(obj, window.oopLib[parentsNames[i]]);
+            }
+        }
+    }
+    return obj;
+}
+
+
+/**
+ * Creating a class by name
+ * @param {string} - name of the class
+ * @param {Object} - config options of the object. Thay will replace same name options in parent class.
+ */
 function createClass(className, config) {
 
     if (!(className in oopLib)) {
@@ -50,11 +122,43 @@ function createClass(className, config) {
         return;
     }
 
-    var obj = oopLib[className];
+    var mainObject = oopLib[className],
+        obj = {},
+        array_parents = [].concat(mainObject.extends || []),
+        order = 0,
+        all_parents = getAllParents(array_parents, [], 0, order),
+        all_parents_arr = unique(all_parents, function(a, b) {
+            if (a.level === b.level && a.value === b.value) {
+                return 0;
+            }
+            return 1;
+        });
 
+    all_parents_arr.sort(function(a, b) {
+        if (a.level != b.level) {
+            return (a.level < b.level);
+        }
+        if (a.order != b.order) {
+            return (a.order < b.order);
+        }
+        return a.value != b.value;
+    });
+
+    var all_parents2 = [];
+
+    for (var i = 0; i < all_parents_arr.length; i++) {
+        all_parents2.push(all_parents_arr[i].value);
+    }
+    if (config.extends) {
+        all_parents2 = all_parents2.concat(config.extends);
+    }
+
+    obj = mixinConfig(all_parents2, obj);
+
+    Object.assign(obj, oopLib[className]);
     Object.assign(obj, config);
+
     obj.className = className;
     obj.init();
     return obj;
 }
-
